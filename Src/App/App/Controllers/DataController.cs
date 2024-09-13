ad18214c5fdf1b525ee19af26f677aa6fd61232d
ethanschafstall <ethans9999@gmail.com>
2024-09-13 13:16:19 +0200

    Create App
    Created the base App folder which contains the ASPNET c# project.

---
+++ Src/App/App/Controllers/DataController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using App.Tools;
+using System.Data;
+
+namespace App.Controllers
+{
+    public class DataController : Controller
+    {
+        // GET: DataController
+        public ActionResult Data()
+        {
+            DataTable dataTable = DataManager.ConvertCsvToDataTable("C:\\Users\\po01imj\\Documents\\P_FUN_323\\Data\\5634-Zeitreihe_Elektrizitätsbilanz_Schweiz_Monatswerte.csv");
+
+            var dataList = DataManager.ConvertDataTableToList(dataTable);
+            dataList.RemoveRange(0, 9);
+            return View(dataList);
+        }
+
+        // GET: DataController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: DataController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DataController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DataController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: DataController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DataController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DataController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Src/App/App/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿const displayData = () => {
+    $.ajax({
+        type: "GET",
+        url: "/Home/testResult",
+        dataType: "json"
+    }).done(function (result) {
+        console.log(result)
+    })
+}
