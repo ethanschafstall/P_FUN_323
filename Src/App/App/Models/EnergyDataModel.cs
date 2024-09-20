b0bec35d8589a33a27a7c044d977810c15aa9656
ethanschafstall <ethans9999@gmail.com>
2024-09-20 13:35:29 +0200

    feat (energy data model)
    Defines how an energy data type must be structured

---
+++ Src/App/App/Models/EnergyDataModel.cs
@@ -0,0 +1,19 @@
+namespace App.Models
+{
+    public class EnergyDataModel
+    {
+        public string MonthYear { get; set; }
+        public int? Hydropower { get; set; }
+        public int? NuclearPower { get; set; }
+        public int? ThermalPower { get; set; }
+        public int? TotalProduction { get; set; }
+        public int? PumpingStorage { get; set; }
+        public int? NetProduction { get; set; }
+        public int? Import { get; set; }
+        public int? Export { get; set; }
+        public int? DomesticConsumption { get; set; }
+        public int? Losses { get; set; }
+        public int? FinalConsumption { get; set; }
+        public int? ExportImportBalance { get; set; }
+    }
+}
