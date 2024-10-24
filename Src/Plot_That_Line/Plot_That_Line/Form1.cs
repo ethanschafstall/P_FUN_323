21b7bd90f2a62181ad8bb103c74fe5e5c86956e0
ethanschafstall <ethans9999@gmail.com>
2024-10-24 11:32:03 +0200

    refactor(project): Creates new C# windows form app project

---
+++ Src/Plot_That_Line/Plot_That_Line/Form1.cs
@@ -0,0 +1,20 @@
+﻿using LiveCharts.Wpf;
+using LiveCharts;
+using System;
+using System.Windows.Forms;
+
+using System.Windows.Media;
+using System.Collections.Generic;
+
+
+namespace Plot_That_Line
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
