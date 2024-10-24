46d36a3d0a9b46621bc8faa9411f0e6bcae71890
ethanschafstall <ethans9999@gmail.com>
2024-10-24 11:32:59 +0200

    feat(Models): Adds EnergyDataModel class

---
+++ Src/Plot_That_Line/Plot_That_Line/Models/EnergyDataModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Plot_That_Line.Models
+{
+    public class EnergyData
+    {
+        public int? Month { get; set; }
+        public int? Year { get; set; }
+        public int? Hydropower { get; set; }
+        public int? NuclearPower { get; set; }
+        public int? ThermalPower { get; set; }
+        public int? TotalProduction { get; set; }
+        public int? PumpingStorage { get; set; }
+        public int? NetProduction { get; set; }
+        public int? Import { get; set; }
+        public int? Export { get; set; }
+        public int? DomesticConsumption { get; set; }
+        public int? Losses { get; set; }
+        public int? FinalConsumption { get; set; }
+        public int? ExportImportBalance { get; set; }
+    }
+}
