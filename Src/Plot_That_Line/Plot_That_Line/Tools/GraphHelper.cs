2fde727579924a45161b325f3dc6580803ab0aa5
ethanschafstall <ethans9999@gmail.com>
2024-10-25 12:19:44 +0200

    feat(graph helper): adds class for creating graphs to be displayed via the view

---
+++ Src/Plot_That_Line/Plot_That_Line/Tools/GraphHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plot_That_Line.Tools
+{
+    internal class GraphManager
+    {
+    }
+}
