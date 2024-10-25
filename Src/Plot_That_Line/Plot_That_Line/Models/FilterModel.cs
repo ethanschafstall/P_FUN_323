b061703cd0ab3eb130d6121892e5a5151cb1ca1a
ethanschafstall <ethans9999@gmail.com>
2024-10-25 11:51:02 +0200

    feat(filter model): adds a filter model class for sorting data
    filter objects will be created by the view filter section and used by the controller to filtered data.

---
+++ Src/Plot_That_Line/Plot_That_Line/Models/FilterModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plot_That_Line.Models
+{
+    internal class FilterModel
+    {
+        public int? StartYear { get; set; }
+        public int? EndYear { get; set; }
+        public List<int> SelectedMonths { get; set; }
+        public bool ShowHydro { get; set; }
+        public bool ShowThermal { get; set; }
+        public bool ShowNuclear { get; set; }
+    }
+}
