5ecf5660bc4ac464481936f9b67436960fa1462a
ethanschafstall <ethans9999@gmail.com>
2024-10-25 11:44:33 +0200

    feat(viewmodel): Creates a energydata viewmodel class
    Creates objects to be used by the view to generate the graph

---
+++ Src/Plot_That_Line/Plot_That_Line/ViewModels/EnergyDataViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plot_That_Line.ViewModels
+{
+    internal class EnergyDataViewModel
+    {
+        public int Year { get; set; }
+        public double? HydroProduction { get; set; }
+        public double? ThermalProduction { get; set; }
+        public double? NulcearProduction { get; set; }
+        public double? TotalProduction { get; set; }
+    }
+}
