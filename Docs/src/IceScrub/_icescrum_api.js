39be887cc5d862fb361452dc38fb818af2e9be26
ethanschafstall <ethans9999@gmail.com>
2024-11-01 20:54:52 +0100

    fix(icescrub): Removes token

--- Docs/src/IceScrub/_icescrum_api.js
+++ Docs/src/IceScrub/_icescrum_api.js
@@ -1,6 +1,6 @@
 // A token is required to authenticate your requests
 // Get it from IceScrum > MyAccount > Account > API tokens
-var iceScrumToken = '693d543668e246849b7e10da9f61e02d' 
+var iceScrumToken = '' 
 
 // You can use IceScrub utils with the following list of projects
 // Get their IDs from each project > Settings > Your Project > Key
